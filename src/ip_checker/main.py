3e6ef8f0a8d098083bc8e29a9d2e3dad54dc5216
Jaskirat Singh <jaskirat0810@gmail.com>
2021-01-05 14:29:17 +0530

    Added code for virus total API

--- src/ip_checker/main.py
+++ src/ip_checker/main.py
@@ -1,11 +1,11 @@
 from typing import List
 import pandas as pd
 import sys
-# sys.path.append("/malicious-ip-checker/src/")
 
 from src.ip_checker.args import parse_args
 from src.ip_checker.reader import Reader
 from src.ip_checker.writer import Writer
+from src.ip_checker.apis.virustotal_api import virustotal_main
 
 
 def main(argv: List) -> None:
@@ -21,7 +21,9 @@ def main(argv: List) -> None:
 def run_main(ip_addresses_file_path: str) -> pd.DataFrame:
     reader: Reader = Reader(file_path=ip_addresses_file_path)
     ip_addresses: pd.DataFrame = reader.read_csv()
-    print(ip_addresses.head())
+
+    for index in range(len(ip_addresses)):
+        print(virustotal_main(ip_addresses.loc[index, "ip_address"]))
 
     return ip_addresses
 
